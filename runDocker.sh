d7b288d147b64e445d8e97c3dcdce00d36bfd02c
Pablo Alba <pablo.alba@kaleidos.net>
2016-03-07 01:03:54 +0100

    Stating ionic project

---
+++ runDocker.sh
@@ -0,0 +1,1 @@
+docker run -ti -p 8100:8100 -p 35729:35729 -v /home/dodger/proyectos/piweek/eve2/src:/myApp gleclaire/ionic-framework /bin/bash
---
+++ src/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ionic serve --all --port 8100 --livereload-port 35729
+
