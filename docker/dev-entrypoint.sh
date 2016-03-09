247c928d2597001b5cf1e576502d5c090765dc52
Miguel de la Cruz <mgdelacroix@gmail.com>
2016-03-09 22:12:45 +0100

    Modify the way ionic is run based on the start.sh script

--- docker/dev-entrypoint.sh
+++ docker/dev-entrypoint.sh
@@ -5,7 +5,7 @@ tmux -2 new-session -d -s eve
 tmux new-window -t eve:1 -n 'ionic'
 tmux select-window -t eve:1
 tmux send-keys -t eve 'cd app' C-m
-tmux send-keys -t eve 'ionic serve' C-m
+tmux send-keys -t eve 'ionic serve --all --port 8100 --livereload-port 35729' C-m
 
 tmux rename-window -t eve:0 'layout'
 tmux select-window -t eve:0
