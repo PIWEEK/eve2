7b7d470543fd06c55205d6de7c2c6514b17c500d
Miguel de la Cruz <mgdelacroix@gmail.com>
2016-03-09 14:36:25 +0100

    Update docker to use a general path

--- runDocker.sh
+++ runDocker.sh
@@ -1,1 +1,1 @@
-docker run -ti -p 8100:8100 -p 35729:35729 -v /home/dodger/proyectos/piweek/eve2/src:/myApp gleclaire/ionic-framework /bin/bash
+docker run -ti -p 8100:8100 -p 35729:35729 -v `pwd`/src:/myApp gleclaire/ionic-framework /bin/bash
